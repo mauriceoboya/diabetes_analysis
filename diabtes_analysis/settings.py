6592c6a099fe5e6d17a654563c2952d291e8d507
mauriceoboya <mauriq97@gmail.com>
2023-07-17 10:54:44 +0300

    logistic regression part

--- diabtes_analysis/settings.py
+++ diabtes_analysis/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-unkn55-_bbjjh!6wctve*i2e+xdmisa(hrr_fb#mz^^&kao&#g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
