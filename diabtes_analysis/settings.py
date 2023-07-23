21bbbd4cf5fd53fb95d39355cf8173a158989ffb
mauriceoboya <mauriq97@gmail.com>
2023-07-23 13:49:02 +0300

    tailwind

--- diabtes_analysis/settings.py
+++ diabtes_analysis/settings.py
@@ -76,16 +76,16 @@ WSGI_APPLICATION = 'diabtes_analysis.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default':dj_database_url.parse(os.environ.get('DATABASE_URL'))
-#}
-DATABASE={
-    'default': {
-             'ENGINE':'django.db.backend.sqlite3',
-             'NAME':BASE_DIR/'db.sqlite3'
-    }
-    
+DATABASES = {
+    'default':dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
+#DATABASE={
+#    'default': {
+#             'ENGINE':'django.db.backend.sqlite3',
+#             'NAME':BASE_DIR/'db.sqlite3'
+#    }
+    
+#}
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
