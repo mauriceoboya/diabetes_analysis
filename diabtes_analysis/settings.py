fc5344b4a20e031f9dbde22bcb4349827c5c7b1a
mauriceoboya <mauriq97@gmail.com>
2024-08-07 02:22:51 +0300

    updates

--- diabtes_analysis/settings.py
+++ diabtes_analysis/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-unkn55-_bbjjh!6wctve*i2e+xdmisa(hrr_fb#mz^^&kao&#g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
